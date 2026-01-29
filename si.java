ffaa0f6512e754f2a50fcb42bbd4fa6c2e25d1a5
Raj-24680 <rrrj24680@gmail.com>
2026-01-30 01:07:06 +0530

    Create si.java

---
+++ si.java
@@ -0,0 +1,9 @@
+public class si{
+  public static void main (String[] args){
+    int p=1000;
+    int r=2;
+    int t=1;
+    int simpl=(p*r*t)/100;
+    system.out.println("si is:"+simpl);
+  }
+}
