de8010e7f4d2b904a88906e5fbec6a40c99ff6ff
Raj-24680 <rrrj24680@gmail.com>
2026-01-30 00:58:41 +0530

    Create areasquare.java

---
+++ areasquare.java
@@ -0,0 +1,8 @@
+#AS
+public class areasquare {
+    public static void main(String[] args) {
+        int side = 5;   // change value as needed
+        int area = side * side;
+        System.out.println("Area of the square is: " + area);
+    }
+}
