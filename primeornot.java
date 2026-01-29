e6482697b73a14d315a20b2e942974fda9166318
Raj-24680 <rrrj24680@gmail.com>
2026-01-30 01:18:41 +0530

    Refactor prime number checker and rename class
    Refactored prime number checking logic and updated class name.

--- primeornot.java
+++ primeornot.java
@@ -1,19 +1,19 @@
-public class primeornot{
-  public static void main (String[] args){
-    int num=7;
-    boolean isprime=true;
-    if(num<=1){
-      isprime=false;
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        int num = 7;   // change number as needed
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
     }
-    else{
-      for(int i=2;i<=num/2;i++){
-        if(num%i==0){
-          isprime=false;
-          break;
         }
-    }
-}
-  if (isPrime) {
+      
+        if (isPrime) {
             System.out.println(num + " is a Prime number");
         } else {
             System.out.println(num + " is Not a Prime number");
